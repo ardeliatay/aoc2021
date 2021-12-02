5027011e40423c80a9e986c193648084a95424a3
ardeliatay <ardeliatay@gmail.com>
2021-12-02 11:04:39 -0800

    Clean up

--- day1/p1.js
+++ day1/p1.js
@@ -2,11 +2,6 @@ const rawInput = require('./input');
 
 const input = rawInput.split("\n").map(i => Number(i))
 
-const array = []
-input.map((i, idx) => {
-  if (i > input[idx-1]) {
-    array.push('inc');
-  }
-})
+const array = input.filter((i, idx) => i > input[idx-1])
 
  console.log(array.length)
--- day1/p2.js
+++ day1/p2.js
@@ -6,11 +6,6 @@ input.map((i, idx) => {
   array1.push(i+input[idx+1]+input[idx+2])
 })
 
- const array2 = []
- array1.map((i, idx) => {
-   if (i > array1[idx-1]) {
-     array2.push('inc');
-   }
- })
+ const array2 = array1.filter((i, idx) => i > array1[idx-1])
 
 console.log(array2.length)
